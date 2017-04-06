a9943492f10e8c13c4581cd5d2c2de28f51d1d25
SuJoung Kim <abbey17@naver.com>
2017-04-06 14:40:47 +0900

    add precedence function

--- src/main/java/univ/lecture/Calculator.java
+++ src/main/java/univ/lecture/Calculator.java
@@ -4,7 +4,33 @@ package univ.lecture;
  * Created by tchi on 2017. 3. 19..
  */
 public class Calculator {
+	
+	int b;
+	
     public int calculate(String exp) {
         return 2;
     }
+    
+	public int precedence(String token) {
+		switch (token) {
+		case "(":
+			b = 0;
+			break;
+		case "*":// 곱하기
+			b = 2;
+			break;
+		case "/":// 나누기
+			b = 2;
+			break;
+		case "+":// 더하기
+			b = 1;
+			break;
+		case "-":// 빼기
+			b = 1;
+			break;
+
+		}
+		return b;
+	}
+    
 }
